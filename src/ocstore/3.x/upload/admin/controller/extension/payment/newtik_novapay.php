fbaee215a5274d448ed77ac2accd6995df233418
Stanislav93 <stas.stecenko@ukr.net>
2023-08-01 13:04:49 +0300

    1.1.33

--- src/ocstore/3.x/upload/admin/controller/extension/payment/newtik_novapay.php
+++ src/ocstore/3.x/upload/admin/controller/extension/payment/newtik_novapay.php
@@ -6,7 +6,7 @@ class ControllerExtensionpaymentNewtikNovapay extends AdminModuleController {
     protected $code = 'newtik_novapay';
     protected $path = 'extension/payment/newtik_novapay';
     protected $type = 'payment';
-    protected $version = '1.1.32';
+    protected $version = '1.1.33';
     protected $setting;
     
     protected $mod_templeate_view_old = true;
