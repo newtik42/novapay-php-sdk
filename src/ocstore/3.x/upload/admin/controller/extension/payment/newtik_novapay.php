ced8b9b2c0e8574203e1b6dd5d66650c335b3e23
Stanislav93 <stas.stecenko@ukr.net>
2023-08-01 12:58:15 +0300

    1.1.30

--- src/ocstore/3.x/upload/admin/controller/extension/payment/newtik_novapay.php
+++ src/ocstore/3.x/upload/admin/controller/extension/payment/newtik_novapay.php
@@ -6,7 +6,7 @@ class ControllerExtensionpaymentNewtikNovapay extends AdminModuleController {
     protected $code = 'newtik_novapay';
     protected $path = 'extension/payment/newtik_novapay';
     protected $type = 'payment';
-    protected $version = '1.1.29';
+    protected $version = '1.1.30';
     protected $setting;
     
     protected $mod_templeate_view_old = true;
@@ -19,8 +19,8 @@ class ControllerExtensionpaymentNewtikNovapay extends AdminModuleController {
         $data['languages'] = $this->getLanguagesList();
         
         $this->load->model('localisation/order_status');
-        $data['order_statuses'] = $this->model_localisation_order_status->getOrderStatuses();
-        
+        $data['order_statuses_'] = $this->model_localisation_order_status->getOrderStatuses();
+        $data['order_statuses_'] = $data['order_statuses_'];
 
         
         $this->viewOutputIndex($data ?? []);
