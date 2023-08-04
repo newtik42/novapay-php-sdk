1024a09143819705ce8a55a144f7f092d9bb9504
Stanislav93 <stas.stecenko@ukr.net>
2023-08-04 19:23:44 +0300

    1.2.13

--- src/ocstore/3.x/upload/catalog/controller/extension/payment/newtik_novapay.php
+++ src/ocstore/3.x/upload/catalog/controller/extension/payment/newtik_novapay.php
@@ -5,14 +5,17 @@ class ControllerExtensionPaymentNewtikNovapay extends Controller {
     public function index() {
         $this->load->language('extension/module/newtik_novapay');
         
+        $data['button_confirm'] = $this->language->get('button_confirm');
+        $data['button_pay'] = $this->language->get('button_pay');
         
+        $data['path'] = 'extension/module/newtik_novapay';
 
         return $this->load->view('extension/module/newtik_novapay', $data);
     }
     
-    public function getSetting() {
-        $this->config->get('sdfsdf');
-        //$results = 
+    public function createPay() {
+        
+        
         
     }
 
